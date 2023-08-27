3de622d630ee185bfe362f6e32a9cbdbbe0fc417
crazyd2sty <95695102+crazyd2sty@users.noreply.github.com>
2023-08-27 15:01:06 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "utils.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <TlHelp32.h>
 #include <Windows.h>
 
@@ -13,45 +15,46 @@ int main() {
 
     puts("Launch the game from Steam.");
 
-
-    // Loop through the game window
-    HWND l4d2_hwnd = NULL;
-    while ((l4d2_hwnd = FindWindow(NULL, TEXT("Left 4 Dead 2 - Direct3D 9"))) == NULL) {
-        Sleep(1000);
-    }
-
-    puts("Find the window, close the game animation, and intervene in the process after 10 seconds!");
-    Sleep(10000);
-    // Gets the process ID
-    DWORD l4d2_process_id = 0;
-    GetWindowThreadProcessId(l4d2_hwnd, &l4d2_process_id);
-
-    // Open the process
-  HANDLE l4d2_process = OpenProcess(PROCESS_VM_READ, FALSE, l4d2_process_id);
-  if (l4d2_process == NULL || l4d2_process_id == 0) {
-    error("Unable to get process ID!");
+  // Keep looping until we find the l4d2 window.
+  HWND l4d2_hwnd = NULL;
+  while ((l4d2_hwnd = FindWindow(NULL, TEXT("Left 4 Dead 2 - Direct3D 9"))) == NULL) {
+    Sleep(1000);
   }
 
-  // Gets a handle to loop through the loaded modules in the process。
+  puts("Game found!");
+
+  // Get the process id from the window.
+  DWORD l4d2_process_id = 0;
+  GetWindowThreadProcessId(l4d2_hwnd, &l4d2_process_id);
+
+  // Get a handle to the process with virtual memory read permissions.
+  HANDLE l4d2_process = OpenProcess(PROCESS_VM_READ, FALSE, l4d2_process_id);
+  if (l4d2_process == NULL || l4d2_process_id == 0) {
+    error("Could not open process or get process id!");
+  }
+
+  // Get a handle for looping through loaded modules in the process.
   HANDLE hSnapshot = CreateToolhelp32Snapshot(
       TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, l4d2_process_id);
   if (hSnapshot == NULL) {
-    error("Unable to create snapshot!");
+    error("Could not create snapshot!");
   }
 
-  // Obtain the base address .dll client
+  // Get the client.dll base address.
   const DWORD client_base_address = get_client_dll_base_address(hSnapshot);
   CloseHandle(hSnapshot);
   if (client_base_address == 0) {
-    error("The base address of the client .dll could not be found!");
+    error("Could not get client.dll base address!");
   }
 
-  puts("Succeed! Hide the process after five seconds!");
+  puts("Autobhop is now functional!");
+
   // Hide the console
   Sleep(5000);
   HWND console_window = GetConsoleWindow();
   ShowWindow(console_window, SW_HIDE);
-  // Bhop 循环
+
+  // Bhop loop
   DWORD player_base_pointer = 0;
   DWORD m_flags = 0;
   while (TRUE) {
@@ -61,27 +64,29 @@ int main() {
           ExitProcess(0);
       }
 
-    // Check if the spacebar is pressed.
-    if (GetAsyncKeyState(' ') & 0x8000) {
-      // Read the player base pointer
-      ReadProcessMemory(
-          l4d2_process, (char *)client_base_address + player_base_offset,
-          &player_base_pointer, sizeof(player_base_pointer), NULL);
-      // Read the mFlags variable from l4d2
-      ReadProcessMemory(l4d2_process,
-                        (char *)player_base_pointer + m_flags_offset, &m_flags,
-                        sizeof(m_flags), NULL);
+      // Check if the spacebar is pressed.
+      if (GetAsyncKeyState(' ') & 0x8000) {
+          // Read the player base pointer
+          ReadProcessMemory(
+              l4d2_process, (char*)client_base_address + player_base_offset,
+              &player_base_pointer, sizeof(player_base_pointer), NULL);
+          // Read the mFlags variable from l4d2
+          ReadProcessMemory(l4d2_process,
+              (char*)player_base_pointer + m_flags_offset, &m_flags,
+              sizeof(m_flags), NULL);
 
-      // Autobhop main program
-      if (m_flags != 0x80 && m_flags != 0x82 && m_flags != 0x280 &&
-          m_flags != 0x282) {
-        SendMessage(l4d2_hwnd, WM_KEYDOWN, ' ', 0x390000);
-      } else if (m_flags == 0x80 || m_flags == 0x82 || m_flags == 0x280 ||
-                 m_flags == 0x282) {
-        SendMessage(l4d2_hwnd, WM_KEYUP, ' ', 0x390000);
+          // Autobhop main program
+          if (m_flags != 0x80 && m_flags != 0x82 && m_flags != 0x280 &&
+              m_flags != 0x282) {
+              SendMessage(l4d2_hwnd, WM_KEYDOWN, ' ', 0x390000);
+          }
+          else if (m_flags == 0x80 || m_flags == 0x82 || m_flags == 0x280 ||
+              m_flags == 0x282) {
+              SendMessage(l4d2_hwnd, WM_KEYUP, ' ', 0x390000);
+          }
       }
-    }
 
-    Sleep(0);
+      Sleep(0);
   }
 }
+
